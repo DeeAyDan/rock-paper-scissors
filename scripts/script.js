a5f238c2e05311ab1d29c7de5cbfede64085a2b6
Daniel Kromek <kromekdani@gmail.com>
2021-05-29 21:55:41 +0300

    Improved version with buttons and a little CSS

--- scripts/script.js
+++ scripts/script.js
@@ -12,49 +12,56 @@ function computerPlay() {
     }
 
 }
-
+let playerPick;
 let scoreCheck;
-let playerPick = prompt(`Choose your weapon`, ``);
+let playerScore = 0;
+let computerScore = 0;
+
+const para = document.querySelector('.resultText');
+const runningScore = document.querySelector('.runningScore');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', function(e){
+        playerPick = button.id;
+        console.log(playRound());
+        para.textContent = playRound();
+        if(scoreCheck === 1){
+            ++playerScore;
+            runningScore.textContent = `${playerScore} - ${computerScore}`
+        }
+        else if(scoreCheck === 2){
+            ++computerScore;
+            runningScore.textContent = `${playerScore} - ${computerScore}`
+        }
+
+        if(playerScore === 5){
+            runningScore.textContent = `You won the game!`
+        }
+        else if (computerScore === 5){
+            runningScore.textContent = `You lost the game!`
+        }
+        
+
+    })
+    
+})
+
+
 
 // Plays 1 round of rock paper scissors
 function playRound(playerSelection = playerPick, computerSelection = computerPlay()){
-    playerSelection = playerSelection.toLowerCase();
+        scoreCheck = 0
     if (playerSelection === `rock` && computerSelection === `scissors` || playerSelection === `paper` && computerSelection === `rock` || playerSelection === `scissors` && computerSelection === `paper`){
         scoreCheck = 1
         return `You won! ${playerSelection} beats ${computerSelection}.`
     }
     else if (playerSelection === computerSelection){
-        scoreCheck = 2
+        scoreCheck = 0
         return `It's a tie between ${playerSelection} and ${computerSelection}`
     }
     else {
-        scoreCheck = 0
+        scoreCheck = 2
         return `You lost! ${computerSelection} beats ${playerSelection}.`
-    }
-}
-
-// Plays 5 rounds of rock paper scissors and sums the score at the end
-function game(){   
-    let playerScore = 0;
-    let computerScore = 0;
-    for(i = 0 ; i < 5 ; i++){
-       playRound()
-       console.log(playRound())
-        if (scoreCheck === 1){
-            playerScore = playerScore + 1;
-        }
-        else if (scoreCheck === 0){
-            computerScore = computerScore + 1;
-        } 
-    }
-
-    if(playerScore > computerScore){
-        return `You won the 5 round game! The score is ${playerScore} - ${computerScore}`
-    }
-    else if (playerScore < computerScore){
-        return `You lost the 5 round game! The score is ${playerScore} - ${computerScore}`
-    }
-    else {
-        return `It's a tie! The score is ${playerScore} - ${computerScore}`
     }
 }
